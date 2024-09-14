46d6645cbb65c99fbef09fdf21f220611be29c22
Sakshi Vats <sakshi12135@gmail.com>
2024-09-14 15:11:35 +0530

    update new

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import bookingRoute from './routes/bookings.js';
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 8000;
+//const port = process.env.PORT || 8000;
 app.use(cors(
     {
     origin : ["https://tour-frontend-tau.vercel.app"],
